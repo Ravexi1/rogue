4d9fda56a755514ce57d4e904d77cb3e7c1c3c7e
Ravexi1 <raklo2281337@mail.ru>
2025-02-01 17:36:38 +0500

    Player and cinemachine camera

---
+++ rogue/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private float horizontal;
+    private float vertical;
+    public float speed;
+    private Vector2 moveInput;
+
+    [SerializeField] private Rigidbody2D rb;
+    public Animator animator;
+
+    void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
+
+        moveInput = new Vector2(horizontal, vertical).normalized;
+    }
+    void FixedUpdate()
+    {
+        // Передвижение персонажа
+        rb.linearVelocity = moveInput * speed;
+    }
+
+
+}
